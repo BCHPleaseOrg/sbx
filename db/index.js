4307ff68f0fb74b67edd8f23a2d10483bda02443
Bitcoin Please <support@bchplease.org>
2022-07-08 03:36:41 -0400

    Enable OrbitDB creation.

--- db/index.js
+++ db/index.js
@@ -15,17 +15,42 @@ const init = () => {
 /**
  * Main
  */
-// async function main () {
-//     /* Create IPFS instance. */
-//     const ipfsOptions = { repo : './ipfs', }
-//     const ipfs = await IPFS.create(ipfsOptions)
-//
-//     /* Create OrbitDB instance. */
-//     const orbitdb = await OrbitDB.createInstance(ipfs)
-// }
+async function main () {
+    /* Import modules. */
+    const IPFS = await import('ipfs')
+    // const OrbitDB = await import('orbit-db')
+    const OrbitDB = require('orbit-db')
+    // console.log('\nOrbitDB', OrbitDB);
+
+    try {
+        /* Create IPFS instance. */
+        // TODO: Add ENV config option.
+        const ipfsOptions = {
+            repo: './ipfs',
+        }
+        const ipfs = await IPFS.create(ipfsOptions)
+        // console.log('\nIPFS', ipfs);
+
+        /* Request IPFS ID. */
+        const ipfsid = await ipfs.id()
+
+        const orbitOptions = {
+            id: ipfsid.id.toString(),
+            directory: './orbitdb',
+        }
+
+        /* Create OrbitDB instance. */
+        const orbitdb = await OrbitDB.createInstance(ipfs, orbitOptions)
+        // console.log('ORBITDB', orbitdb);
+
+        console.info('\nIPFS & OrbitDB are now ready...')
+    } catch (e) {
+        console.log('ERROR', e)
+    }
+}
 
 init()
 
-// main()
+main()
 
-require('./Orders')
+// require('./Orders')
