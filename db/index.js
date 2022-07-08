b600903506d2cb7a6ce536c7a3c9d5f1104733f9
Bitcoin Please <support@bchplease.org>
2022-07-08 04:13:05 -0400

    Add startup banner.

--- db/index.js
+++ db/index.js
@@ -2,13 +2,54 @@
 // const IPFS = require('ipfs')
 // const OrbitDB = require('orbit-db')
 
+const banner1 = () => {
+    console.log(`
+  Smart Bitcoin Exchange
+  Federation Database
+
+  ███████╗██████╗ ██╗  ██╗    ██████╗ ██████╗
+  ██╔════╝██╔══██╗╚██╗██╔╝    ██╔══██╗██╔══██╗
+  ███████╗██████╔╝ ╚███╔╝     ██║  ██║██████╔╝
+  ╚════██║██╔══██╗ ██╔██╗     ██║  ██║██╔══██╗
+  ███████║██████╔╝██╔╝ ██╗    ██████╔╝██████╔╝
+  ╚══════╝╚═════╝ ╚═╝  ╚═╝    ╚═════╝ ╚═════╝
+                                    v${require('./package.json').version}
+    `)
+}
+
+const banner2 = () => {
+    console.log(`
+  Smart Bitcoin Exchange
+  Federation Database
+
+     d888888o.   8 888888888o \`8.\`8888.      ,8'        8 888888888o.      8 888888888o
+   .\`8888:' \`88. 8 8888    \`88.\`8.\`8888.    ,8'         8 8888    \`^888.   8 8888    \`88.
+   8.\`8888.   Y8 8 8888     \`88 \`8.\`8888.  ,8'          8 8888        \`88. 8 8888     \`88
+   \`8.\`8888.     8 8888     ,88  \`8.\`8888.,8'           8 8888         \`88 8 8888     ,88
+    \`8.\`8888.    8 8888.   ,88'   \`8.\`88888'            8 8888          88 8 8888.   ,88'
+     \`8.\`8888.   8 8888888888     .88.\`8888.            8 8888          88 8 8888888888
+      \`8.\`8888.  8 8888    \`88.  .8'\`8.\`8888.           8 8888         ,88 8 8888    \`88.
+  8b   \`8.\`8888. 8 8888      88 .8'  \`8.\`8888.          8 8888        ,88' 8 8888      88
+  \`8b.  ;8.\`8888 8 8888    ,88'.8'    \`8.\`8888.         8 8888    ,o88P'   8 8888    ,88'
+   \`Y8888P ,88P' 8 888888888P .8'      \`8.\`8888.        8 888888888P'      8 888888888P
+                                                                      v${require('./package.json').version}
+    `)
+}
+
 /**
  * Initialization
  */
 const init = () => {
+    /* Spacer. */
     console.log()
-    console.log('Starting the SBX Federation database..')
-    console.log('--------------------------------------------------')
+
+    /* Show (intro) banner. */
+    banner1()
+
+    /* Startup messages. */
+    console.log('  Initializing new IPFS + OrbitDB instances.')
+    console.log('  Please wait...')
+    console.log('___________________________________________________________________________')
     console.log()
 }
 
@@ -34,6 +75,11 @@ async function main () {
         /* Request IPFS ID. */
         const ipfsid = await ipfs.id()
 
+        console.log('___________________________________________________________________________')
+        console.log()
+        console.info('  IPFS is ready to go!')
+        console.info('  ID -> ' + ipfsid.id.toString())
+
         const orbitOptions = {
             id: ipfsid.id.toString(),
             directory: './orbitdb',
@@ -43,7 +89,10 @@ async function main () {
         const orbitdb = await OrbitDB.createInstance(ipfs, orbitOptions)
         // console.log('ORBITDB', orbitdb);
 
-        console.info('\nIPFS & OrbitDB are now ready...')
+        console.log('___________________________________________________________________________')
+        console.log()
+        console.info('  OrbitDB is ready to go!')
+        console.log('  ID -> ' + '...') // TODO: Add ID.
     } catch (e) {
         console.log('ERROR', e)
     }
