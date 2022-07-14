644c2ee07491fd722d74cbf3d316480ef916a907
Bitcoin Please <support@bchplease.org>
2022-07-14 06:42:46 -0400

    Add crypto icons.

--- manager/src/renderer/screens/Assets.tsx
+++ manager/src/renderer/screens/Assets.tsx
@@ -2,6 +2,9 @@ import Header from '../components/Header'
 
 import icon from '../../../assets/logo.png'
 
+import bchIcon from 'cryptocurrency-icons/svg/icon/bch.svg'
+import btcIcon from 'cryptocurrency-icons/svg/icon/btc.svg'
+
 const Assets = () => {
     return (
         <main>
@@ -11,6 +14,42 @@ const Assets = () => {
                 <h1 className="text-5xl text-yellow-700 font-bold">
                     Assets Template
                 </h1>
+
+                <p>
+                    Bridged assets will be denoted with the suffix ".b".
+                </p>
+            </section>
+
+            <section className="mx-3">
+                <div className="mt-3 bg-pink-300 border-2 border-pink-500 rounded-lg">
+                    <img src={bchIcon} className="w-16 text-blue-500" />
+                    Smart Bitcoin (sBCH) --- Bitcoin Cash (BCH)
+                    <img src={btcIcon} className="w-16 text-blue-500" />
+                </div>
+
+                <div className="mt-3 bg-pink-300 border-2 border-pink-500 rounded-lg">
+                    Smart Bitcoin (sBCH) --- Bitcoin (BTC.b)
+                </div>
+
+                <div className="mt-3 bg-pink-300 border-2 border-pink-500 rounded-lg">
+                    Smart Bitcoin (sBCH) --- Ethereum (ETH.b)
+                </div>
+
+                <div className="mt-3 bg-pink-300 border-2 border-pink-500 rounded-lg">
+                    Smart Bitcoin (sBCH) --- Binance USD (BUSD.b)
+                </div>
+
+                <div className="mt-3 bg-pink-300 border-2 border-pink-500 rounded-lg">
+                    Smart Bitcoin (sBCH) --- Dai (DAI.b)
+                </div>
+
+                <div className="mt-3 bg-pink-300 border-2 border-pink-500 rounded-lg">
+                    Smart Bitcoin (sBCH) --- Tether (BUSD.b)
+                </div>
+
+                <div className="mt-3 bg-pink-300 border-2 border-pink-500 rounded-lg">
+                    Smart Bitcoin (sBCH) --- USD Coin (USDC.b)
+                </div>
 
             </section>
 
